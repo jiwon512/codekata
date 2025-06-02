c0f82ad52667dff68b0920ac335f1f87b95cd437
jiwon512 <we5652123@gmail.com>
2025-06-02 19:14:05 +0900

    Create 2. 두 수의 곱.py

---
+++ Python/2. 두 수의 곱.py
@@ -0,0 +1,9 @@
+'''
+문제 : 두 수의 곱 구하기
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120804
+날짜 : 2025-06-02
+'''
+
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
