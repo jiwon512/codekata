4b000f96621eafffd39d6bcecb82cb182cafa43f
jiwon512 <we5652123@gmail.com>
2025-06-02 19:16:31 +0900

    Create 3. 몫 구하기.py

---
+++ Python/3. 몫 구하기.py
@@ -0,0 +1,9 @@
+'''
+문제 : 몫 구하기
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120805
+날짜 : 2025-06-02
+'''
+
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
