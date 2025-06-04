941595bca6589b4de36f9a4f46310ac462e14e0a
jiwon512 <we5652123@gmail.com>
2025-06-04 15:34:36 +0900

    Create 011. 짝수와 홀수.py

---
+++ Python/011. 짝수와 홀수.py
@@ -0,0 +1,11 @@
+'''
+문제 : 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12937
+날짜 : 2025-06-04
+'''
+
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
