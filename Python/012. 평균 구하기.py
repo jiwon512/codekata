0177976f2629df3dc8b0035465bd0d42ea52d908
jiwon512 <we5652123@gmail.com>
2025-06-04 15:43:10 +0900

    Create 012. 평균 구하기.py

---
+++ Python/012. 평균 구하기.py
@@ -0,0 +1,10 @@
+'''
+문제 : 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12944
+날짜 : 2025-06-04
+'''
+
+import numpy as np
+
+def solution(arr):
+    return np.mean(arr)
