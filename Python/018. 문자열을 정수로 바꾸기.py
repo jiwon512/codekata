07168cf8c91497bf6afbd90c93cc7ddf4958c3a3
jiwon512 <we5652123@gmail.com>
2025-06-05 20:03:27 +0900

    Create 018. 문자열을 정수로 바꾸기.py

---
+++ Python/018. 문자열을 정수로 바꾸기.py
@@ -0,0 +1,9 @@
+'''
+문제 : 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12925
+날짜 2025-06-05
+'''
+
+def solution(s):
+    answer = int(s)
+    return answer
